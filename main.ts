ee2c9a69460b09c8f94b86452c1219a105629999
ssande-fwd <ssande@forwardedu.com>
2025-11-19 16:19:26 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-// --- Button A Press Handler (Toggles Mode) ---
-input.onButtonPressed(Button.A, function () {
-	
-})
-// --- Forever Loop (Runs Animation) ---
-basic.forever(function () {
-	
-})
+basic.showIcon(IconNames.Heart)
