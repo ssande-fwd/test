bb77f048083d812acb3526d333706fc507e9b810
ssande-fwd <ssande@forwardedu.com>
2025-11-19 14:56:53 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
+let test = 0
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(1000)
     basic.showIcon(IconNames.SmallHeart)
     basic.pause(1000)
+    test += 1
+    basic.showNumber(test)
 })
